a87acc527cfcc666b7482a4f378f3a8633a596db
dimitur <dimitur@infinitelambda.com>
2022-07-27 18:09:35 +0100

    changed model

--- models/mart/fct_estimated_electricity_usage_carbon_emission.sql
+++ models/mart/fct_estimated_electricity_usage_carbon_emission.sql
@@ -4,7 +4,7 @@ config(
   )
 }}
 
---{% call to a macro that determines where a the client has locations %}
+-- call to a macro that determines where a the client has locations
 /* it checks if locations are in any of three categories:
 
 * UK-level - this is only for the UK since we take into consideration hourly electricity demand only in that country
@@ -17,8 +17,13 @@ uk_locations: BOOL
 region_data_locations: BOOL
 country_data_locations: BOOL
 */
+{% set regions_list = area_checker_macro() %}
+{% set region_uk = regions_list[0] %}
+{% set region_other = regions_list[1] %}
+{% set country = regions_list[2] %}
+
 with
---{% jinja that activates UK-only parts if uk_locations = True %}
+{% if region_uk %}
 average_hourly_intensity AS ( --UK-only
 SELECT
   to_date(FROM_DATE) AS INTENSITY_DATE,
@@ -63,99 +68,91 @@ company_locations as (  --can be general but we need to remove factored_location
   END AS PREMISE_TYPE_FOR_JOINING --when the client has location types for which we do not have factors, we just use the Office factors
   FROM {{ ref('stg_operational_data__company_locations') }} CD
   LEFT JOIN factored_locations FL on FL.PREMISE_TYPE=cd.LOCATION
-)--,
---uk_level_emissions as (
+),
+uk_level_emissions as (
 SELECT  --UK-only
   ac.INTENSITY_DATE as DATE,
   cd.LOCATION_SIZE_SQ_FT,
   cd.LOCATION_ID,
   cd.LOCATION_LATITUDE,
   cd.LOCATION_LONGITUDE,
-  --CD.LOCATION_COUNTRY, --this was not present originally but will be included
+  CD.LOCATION_COUNTRY, --this was not present originally but will be included
   ac.REGION_NAME as CARBON_INTENSITY_REGION_NAME,
   dem.TYPE AS LOCATION_CATEGORY,
   ROUND(cd.LOCATION_SIZE_SQ_FT*(dem.ANNUAL_KWH_DEMAND/365), 2) as ESTIMATED_KWH_USAGE,
   ROUND(cd.LOCATION_SIZE_SQ_FT*(dem.ANNUAL_KWH_DEMAND/365)*"WEIGHTED_DAILY_KGCO2",3) AS TOTAL_KGCO2E
 FROM company_locations CD
 LEFT JOIN {{ ref('stg_additional_modelling_resources__us_commercial_buildings_demand') }} dem ON upper(dem.TYPE)=upper(cd.LOCATION)
+LEFT JOIN {{ ref('stg_additional_modelling_resources__carbon_intensity_regions_mapped') }} r ON r.REGION_NAME=cd.REGION
+LEFT JOIN weighted_daily_intensity ac on ac.REGION_NAME=r.REGION_NAME and cd.PREMISE_TYPE_FOR_JOINING = ac.PREMISE_TYPE
+WHERE CD.LOCATION_COUNTRY = 'United Kingdom' --this is added so we don't factor in non-UK locations
+)
+{% endif %}
 
-/*
-We can start asking clients to provide what region their locations fall in.
-Instead of mapping postcodes to carbon intensity regions, stg_additional_modelling_resources__carbon_intensity_regions_mapped
-can get changed to have the following data:
- - REGION_NAME - name of area for which we have data. This can be UK/US/China/Canada/Australia carbon intensity region, or a country (e.g. France, Bulgaria, etc)
- - REGION_TYPE - whether it is a carbon intensity region or a country.
-*/
---LEFT JOIN {{ ref('stg_additional_modelling_resources__carbon_intensity_regions_mapped') }} r ON r.REGION_NAME=cd.REGION
---LEFT JOIN weighted_daily_intensity ac on ac.REGION_NAME=r.REGION_NAME and cd.PREMISE_TYPE_FOR_JOINING = ac.PREMISE_TYPE
---WHERE CD.LOCATION_COUNTRY = 'United Kingdom' --this is added so we don't factor in non-UK locations
---)
---{% end of jinja that activates UK-only parts if uk_locations = True %}
+{% if region_uk and country %}
+,
+{% endif %}
 
---{% jinja that activates the below comma if uk_locations = True and country_data_locations=True %}
---,
---{% end of jinja that activates the comma if uk_locations = True and country_data_locations=True %}
+{% if region_uk or country %}
+date_spine AS (
+  {{dbt_utils.date_spine("day", "to_date('1970-01-01')", "current_date()")}}
+),
+date_spine_filtered as (
+  SELECT
+  DATE_DAY AS DATE
+FROM date_spine
+WHERE date_day >= '{{ var('start_date') }}' --we can provide this variable so that users without invoiced data operating in locations with no carbon intensity daily data can specify a time range
+)
+{% endif %}
 
---{% jinja that activates if country_data_locations=True AND/OR region_data_locations=True %}
--- date_spine AS (
---   {{dbt_utils.date_spine("day", "to_date('1970-01-01')", "current_date()")}}
--- ),
--- date_spine_filtered as (
---   SELECT
---   DATE_DAY AS DATE
--- FROM date_spine
--- WHERE date_day >= '{{ var('start_date') }}' --we can provide this variable so that users without invoiced data operating in locations with no carbon intensity daily data can specify a time range
--- )
---{% end of jinja that activates if country_data_locations=True AND/OR region_data_locations=True %}
+{% if country %}
+,country_level_emissions as (
+SELECT
+DSF.DATE as DATE,
+CD.LOCATION_SIZE_SQ_FT,
+CD.LOCATION_ID,
+CD.LOCATION_LATITUDE,
+CD.LOCATION_LONGITUDE,
+CD.LOCATION_COUNTRY, --include this in other models
+NULL as CARBON_INTENSITY_REGION_NAME, --we could potentially just put the country name here again
+DEM.TYPE AS LOCATION_CATEGORY,
+ROUND(CD.LOCATION_SIZE_SQ_FT*(DEM.ANNUAL_KWH_DEMAND/365), 2) AS ESTIMATED_KWH_USAGE,
+ROUND(CD.LOCATION_SIZE_SQ_FT*(DEM.ANNUAL_KWH_DEMAND/365)*PUR.TOTAL_KGCO2E,3) AS TOTAL_KGCO2E
+FROM {{ ref('stg_operational_data__company_locations') }} CD
+CROSS JOIN date_spine_filtered DSF
+LEFT JOIN {{ ref('stg_additional_modelling_resources__us_commercial_buildings_demand') }} DEM ON UPPER(DEM.TYPE)=UPPER(CD.LOCATION)
+LEFT JOIN {{ ref('stg_emissions_factors__purchased_electricity_country_level') }} PUR ON UPPER(PUR.COUNTRY)=UPPER(CD.LOCATION_COUNTRY) --causes double join
+WHERE CD.LOCATION_COUNTRY NOT IN ('United Kingdom', 'US', 'China', 'Canada', 'Australia') --removes countries for which we have region-level data
+AND PUR.SCOPE_2_METHODOLOGY = 'LOCATION_BASED'  --this model will only consider LOCATION_BASED emissions for now. I will need to find additional data for other countries and include it in the seed
+)
+{% endif %}
 
---{% jinja that activates if country_data_locations=True %}
---,country_level_emissions as (
--- SELECT
--- DSF.DATE as DATE,
--- CD.LOCATION_SIZE_SQ_FT,
--- CD.LOCATION_ID,
--- CD.LOCATION_LATITUDE,
--- CD.LOCATION_LONGITUDE,
--- CD.LOCATION_COUNTRY, --include this in other models
--- NULL as CARBON_INTENSITY_REGION_NAME, --we could potentially just put the country name here again
--- DEM.TYPE AS LOCATION_CATEGORY,
--- ROUND(CD.LOCATION_SIZE_SQ_FT*(DEM.ANNUAL_KWH_DEMAND/365), 2) AS ESTIMATED_KWH_USAGE,
--- ROUND(CD.LOCATION_SIZE_SQ_FT*(DEM.ANNUAL_KWH_DEMAND/365)*PUR.TOTAL_KGCO2E,3) AS TOTAL_KGCO2E
--- FROM {{ ref('stg_operational_data__company_locations') }} CD
--- CROSS JOIN date_spine_filtered DSF
--- LEFT JOIN {{ ref('stg_additional_modelling_resources__us_commercial_buildings_demand') }} DEM ON UPPER(DEM.TYPE)=UPPER(CD.LOCATION)
--- LEFT JOIN {{ ref('stg_emissions_factors__purchased_electricity_country_level') }} PUR ON UPPER(PUR.COUNTRY)=UPPER(CD.LOCATION_COUNTRY) --causes double join
--- WHERE CD.LOCATION_COUNTRY NOT IN ('United Kingdom', 'US', 'China', 'Canada', 'Australia') --removes countries for which we have region-level data
--- AND PUR.SCOPE_2_METHODOLOGY = 'LOCATION_BASED'  --this model will only consider LOCATION_BASED emissions for now. I will need to find additional data for other countries and include it in the seed
--- )
---{% end of jinja that activates if country_data_locations=True %}
+{% if (region_uk or country) and region_other %}
+,
+{% endif %}
 
---{% jinja that activates the below comma if (uk_locations=True AND/OR country_data_locations=True) AND region_data_locations=True %}
---,
---{% end of jinja that activates the comma if (uk_locations=True AND/OR country_data_locations=True) AND region_data_locations=True %}
-
---{% jinja that activates if region_data_locations=True %}
+{% if region_other %}
 -- region_level_emissions as (
 --[sql query to be added, will be very similar to the one in country_level_emissions CTE]
 -- )
---{% end of jinja that activates if region_data_locations=True %}
+{% endif %}
 
---{% jinja that activates if uk_locations=True %}
--- SELECT * FROM uk_level_emissions
---{% end of jinja that activates if uk_locations=True %}
+{% if region_uk %}
+SELECT * FROM uk_level_emissions
+{% endif %}
 
---{% jinja that activates a UNION if uk_locations=True and (region_data_locations=True AND/OR country_data_locations=True) %}
--- UNION
---{% end of jinja that activates a UNION if uk_locations=True and (region_data_locations=True AND/OR country_data_locations=True) %}
+{% if region_uk and (region_other or country) %}
+UNION
+{% endif %}
 
---{% jinja that activates if country_data_locations=True %}
---SELECT * FROM country_level_emissions
---{% end of jinja that activates if country_data_locations=True %}
+{% if country %}
+SELECT * FROM country_level_emissions
+{% endif %}
 
---{% jinja that activates a UNION uk_locations, region_data_locations and country_data_locations are True  %}
--- UNION
---{% end of jinja that activates a UNION uk_locations, region_data_locations and country_data_locations are True %}
+{% if region_uk and region_other and country %}
+--UNION --commented out until query added
+{% endif %}
 
---{% jinja that activates if region_data_locations=True %}
---SELECT * FROM region_level_emissions
---{% end of jinja that activates if region_data_locations=True %}
+{% if region_other %}
+--SELECT * FROM region_level_emissions  --commented out until query added
+{% endif %}
